9d177adc85018293a6cc50383809bbc6433d714c
lecanardcolvert <59675854+lecanardcolvert@users.noreply.github.com>
2022-11-23 20:24:01 -0500

    fix: ensure correct rollback with Transactional annotation

--- src/test/java/net/lemixeur/repository/UserRepositoryTests.java
+++ src/test/java/net/lemixeur/repository/UserRepositoryTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@Rollback
+@Transactional
 public class UserRepositoryTests {
 
     @Autowired
@@ -48,7 +48,9 @@ public class UserRepositoryTests {
 
         Iterable<User> actualUsers = repository.findAll();
 
-        assertThat(actualUsers).hasSize(3).contains(expectedUser1, expectedUser2, expectedUser3);
+        assertThat(actualUsers)
+                .hasSize(3)
+                .contains(expectedUser1, expectedUser2, expectedUser3);
     }
 
     @Test
